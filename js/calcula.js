2318b9e719895a069d4fbc2ac4941894cf3c0add
Vhmarega <victorhugomarega@hotmail.com>
2022-09-13 20:42:34 -0300

    2-Adição de responsividade.

--- js/calcula.js
+++ js/calcula.js
@@ -1,77 +1,110 @@
 
 
-function TextAreaToArray(){
+function TextAreaToArray() {
     var textarea = document.getElementById("textarea_intervalo").value;
     var textarea_replaced = textarea.replace(",", ".").replace(/\s/g, '');
 
     //console.log();
     var arr = textarea_replaced.split(';').map(element => element.trim());
-    console.log(arr)
+    //console.log(arr)
     return arr;
 }
 
-function moda(){
-    var arr=TextAreaToArray()
+function moda() {
+    var arr = TextAreaToArray()
     var length = arr.length;
-    var soma = 0;
-    for(var i=0; i<length; i++){
-        soma += parseInt(arr[i])
+    let maior;
+    let maior_aux;
+    for (var i = 0; i < length; i++) {
+        if (i == 0) {
+            maior = parseFloat(arr[i])
+        }
+        else if (length % 2 == 0) {
+            if (parseFloat(arr[i]) >= maior) {
+                maior = parseFloat(arr[i])
+            }
+        } else if (length % 2 != 0) {
+            if (parseFloat(arr[i]) > maior & maior > maior_aux) {
+                maior_aux = parseFloat(arr[i])
+            } else if (parseFloat(arr[i]) < maior_aux & maior > maior_aux) {
+                maior = parseFloat(arr[i])
+            } 
+        }
     }
-    var media = soma/length;    
-    return media.toFixed(2); 
+    let moda
+    if (maior == undefined) {
+        moda = [maior_aux];
+    } else if (maior_aux == undefined) {
+        moda = [maior];
+    } else if(maior == maior_aux){
+        moda = [maior];
+    }else {
+        moda = [maior, maior_aux];
+    }
+    //console.log(moda)    
+    return moda;
 }
 
-function media(){
-    var arr=TextAreaToArray()
+function media() {
+    var arr = TextAreaToArray()
     var length = arr.length;
     var soma = 0;
-    for(var i=0; i<length; i++){
+    for (var i = 0; i < length; i++) {
         soma += parseInt(arr[i])
     }
-    var media = soma/length;    
-    return media.toFixed(2); 
+    var media = soma / length;
+    return media.toFixed(2);
 }
 
-function mediana(){
-    var arr=TextAreaToArray()
+function mediana() {
+    var arr = TextAreaToArray()
     var length = arr.length;
     var soma = 0;
-    for(var i=0; i<length; i++){
+    for (var i = 0; i < length; i++) {
         soma += parseInt(arr[i])
     }
-    var media = soma/length;    
-    return media.toFixed(2); 
+    var media = soma / length;
+    return media.toFixed(2);
 }
 
 
-function calcular(){
+function calcular() {
     var textarea = document.getElementById("textarea_intervalo").value;
     var caixa_resposta = document.getElementById("caixa_resposta")
     var moda_box = document.getElementById("moda")
     var media_box = document.getElementById("media")
     var mediana_box = document.getElementById("mediana")
 
-    if(textarea===undefined ||textarea===""){
+    if (textarea === undefined || textarea === "") {
         moda_box.innerText = 0
         media_box.innerText = 0
         mediana_box.innerText = 0
     }
+    else {
+        let moda_arr = moda()
+        let moda_string
+        if (moda_arr.length === 1) {
+            moda_string = moda_arr[0]
+        } else if (moda_arr.length === 2) {
+            moda_string = moda_arr[0] + " e " + moda_arr[1]
+        }
+        console.log("MODA: " + moda_string)
 
-    if(textarea.match){
+        moda_box.innerText = moda_string;
+        media_box.innerText = media();
+        mediana_box.innerText = mediana();
+    }
 
-    }
-    else{
-        alert("Digite uma sequência numérica válida!")
-    }
-    caixa_resposta.style.visibility="visible";
+    caixa_resposta.style.visibility = "visible";
+
 }
 
-function limpar(){
+function limpar() {
     var textarea = document.getElementById("textarea_intervalo");
-    textarea.textContent="";
-    textarea.value=""
-    
+    textarea.textContent = "";
+    textarea.value = ""
+
     var caixa_resposta = document.getElementById("caixa_resposta")
-    caixa_resposta.style.visibility="hidden";
-    
+    caixa_resposta.style.visibility = "hidden";
+
 }
